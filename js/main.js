2a7c67185e5511e89eabafb926b2e7ca33bcfc8a
kaur12 <kaur12@github.com>
2019-03-16 16:42:05 -0400

    uploading files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+(() => {
+
+	console.log("linked");
+	// var member = [];
+	// member[0] = "Prabhjot Kaur";
+	// member[1] = "Bhatti";
+	// member[2] = 20;
+	// document.getElementById("bio1").innerHTML = member[0] + " " + member.length;
+	
+
+
+
+	
+
+})();
